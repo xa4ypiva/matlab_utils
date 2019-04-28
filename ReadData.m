4f20ca3ff326d39b1e957ed2b5fe946d1b844a9f
Vladislav Khmelnitskii <khmelnitskii.vladislav@gmail.com>
2019-04-28 16:23:46 +0300

    [*] Small fix in ReadData functions

--- ReadData.m
+++ ReadData.m
@@ -1,10 +1,7 @@
 function data = ReadData(fileName, format)
-%READDATA Summary of this function goes here
-%   Detailed explanation goes here
 
-fileInfo = dir(fileName);
 fileId = fopen(fileName);
-data = fread(fileId, fileInfo.bytes, format);
+data = fread(fileId, format);
 fclose(fileId);
 
 end
--- ReadDataIQ.m
+++ ReadDataIQ.m
@@ -1,13 +1,10 @@
 function signal = ReadDataIQ(fileName, iqFormat)
-%READDATAIQ Summary of this function goes here
-%   Detailed explanation goes here
 
-fileInfo = dir(fileName);
 fileId = fopen(fileName);
-data = fread(fileId, fileInfo.bytes, iqFormat);
+data = fread(fileId, iqFormat);
 fclose(fileId);
 
-signal = data(1:2:end-1) + 1i * data(2:2:end);
+signal = complex(data(1:2:end-1), data(2:2:end));
 
 end
 
