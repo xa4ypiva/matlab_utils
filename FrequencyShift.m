055d193faf7d85ff1829736a6f70a720b8ea0d36
Vladislav Khmelnitskii <khmelnitskii.vladislav@gmail.com>
2020-04-22 16:24:19 +0300

    [*] Simplified FrequencyShift behaviour

--- FrequencyShift.m
+++ FrequencyShift.m
@@ -1,12 +1,7 @@
 function [signalOut] = FrequencyShift(signalIn, fOffset, fs)
 
 time = (0 : length(signalIn)-1) / fs;
-
-if (~isreal(signalIn))
-    signalOut = signalIn .* exp(1i * 2*pi*fOffset .* time);
-else
-    signalOut = hilbert(signalIn) .* exp(1i * 2*pi*fOffset .* time);
-end
+signalOut = signalIn .* exp(1i * 2*pi*fOffset .* time);
 
 end
 
