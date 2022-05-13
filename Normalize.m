5b081b54e7a3eaa99c3888cbc92bcdcb65f751d6
Vladislav Khmelnitskii <khmelnitskii.vladislav@gmail.com>
2022-05-13 13:04:49 +0300

    [+] #-: Method in Normalize

--- Normalize.m
+++ Normalize.m
@@ -1,4 +1,17 @@
-function [out] = Normalize(in, value_max)
-out = in ./ max([max(real(in)), max(imag(in))]) * value_max;
+function [out] = Normalize(in, value_max, method)
+if (nargin < 3)
+    method = "abs";
+end
+
+switch lower(method)
+    case "abs"
+        norm_coef = value_max / max(abs(in));
+    case "iq"
+        norm_coef = value_max / max([max(abs(real(in))), max(abs(imag(in)))]);
+    otherwise
+        error("method may be [""abs"", ""iq""]");
+end
+
+out = in * norm_coef;
 end
 
