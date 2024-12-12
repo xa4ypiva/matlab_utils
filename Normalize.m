cbeada644222ff5893e41c819153b1d8442bba5f
Vladislav Khmelnitskii <khmelnitskii.vladislav@gmail.com>
2024-12-12 16:54:33 +0300

    [f]: Some fixes

--- Normalize.m
+++ Normalize.m
@@ -5,9 +5,9 @@ end
 
 switch lower(method)
     case "abs"
-        norm_coef = value_max / max(abs(in));
+        norm_coef = value_max / max(abs(in(:)));
     case "iq"
-        norm_coef = value_max / max([max(abs(real(in))), max(abs(imag(in)))]);
+        norm_coef = value_max / max([max(abs(real(in(:)))), max(abs(imag(in(:))))]);
     otherwise
         error("method may be [""abs"", ""iq""]");
 end
